105a0d90e0e95579c29e68200bf7d25e196bf077
RekhuGopal <vrchinnarathod@gmail.com>
2025-01-16 23:31:19 +0530

    Updated Chage

--- AzureStorageQueueTriggerFunction/__init__.py
+++ AzureStorageQueueTriggerFunction/__init__.py
@@ -1,7 +1,6 @@
 import azure.functions as func
 import logging
 import datetime
-from typing import Optional, Dict, Any
 
 def main(msg: func.QueueMessage) -> None:
     message = msg.get_body().decode('utf-8')
